7d342b2f1dae76f7c0aa37abea49a68357bd29e2
Keryu7 <yyurikt@tut.by>
2024-10-21 15:38:18 +0200

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,53 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const input = { a: 7, b: -9, action: Action.Add };
+    const result = simpleCalculator(input);
+    expect(result).toBe(-2);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const input = { a: 32, b: 17, action: Action.Subtract };
+    const result = simpleCalculator(input);
+    expect(result).toBe(15);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const input = { a: 7, b: 3, action: Action.Multiply };
+    const result = simpleCalculator(input);
+    expect(result).toBe(21);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const input = { a: 21, b: 3, action: Action.Divide };
+    const result = simpleCalculator(input);
+    expect(result).toBe(7);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const input = { a: 2, b: 3, action: Action.Exponentiate };
+    const result = simpleCalculator(input);
+    expect(result).toBe(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const input = { a: 3, b: 5, action: 'invalid_action' };
+    const result = simpleCalculator(input);
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const invalidInputs = [
+      { a: '3', b: 5, action: Action.Add },
+      { a: 3, b: null, action: Action.Subtract },
+      { a: {}, b: 5, action: Action.Multiply },
+      { a: 3, b: 5, action: undefined },
+    ];
+
+    invalidInputs.forEach(input => {
+      const result = simpleCalculator(input);
+      expect(result).toBeNull();
+    });
   });
 });
